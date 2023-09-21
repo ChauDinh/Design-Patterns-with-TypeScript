10b9e7db0e1bfe915ea1ed422cf20fdfd2cb71d0
Chau Dinh <chaudvb95uc@gmail.com>
2023-09-21 22:30:22 +0700

    main: builder, factory method patterns

---
+++ factory_method/factory_method-class.ts
@@ -0,0 +1,43 @@
+import fs from 'fs';
+
+abstract class DirectoryScraper {
+  constructor(public dirPath: string) {}
+
+  scanFiles() {
+    return fs
+      .readdirSync(this.dirPath)
+      .reduce<Record<string, unknown>>(
+        (acc: Record<string, unknown>, file: string) => {
+          if (this.isJSONFile(file)) {
+            acc[file] = this.readJSON(`${this.dirPath}/${file}`);
+          } else {
+            acc[file] = this.readText(`${this.dirPath}/${file}`);
+          }
+          return acc;
+        },
+        {}
+      );
+  }
+
+  abstract isJSONFile(file: string): boolean;
+  abstract readJSON(file: string): unknown;
+  abstract readText(file: string): string;
+}
+
+class FileReader extends DirectoryScraper {
+  isJSONFile(file: string): boolean {
+    return file.endsWith('.json');
+  }
+
+  readText(file: string): string {
+    return fs.readFileSync(file, 'utf-8').toString();
+  }
+
+  readJSON(file: string): unknown {
+    return JSON.parse(fs.readFileSync(file, 'utf-8').toString());
+  }
+}
+
+const dirScraper = new FileReader('./data');
+const output = dirScraper.scanFiles();
+console.log('🚀 ~ file: builder-class.ts:46 ~ output:', output);
